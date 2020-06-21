700409433712057430795796c74811eb48ab9e7a
tom <tom@HAL-kubuntu>
2020-06-21 17:52:04 +0100

    incredible update

--- bewwbs.js
+++ bewwbs.js
@@ -1,27 +1,56 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const gis = require('g-i-s');
+const https = require('https');
 
 
 
 client.on('message', (receivedMessage) => {
-    // Prevent bot from responding to its own messages
-    if (receivedMessage.content != 'bewwbs!') {
-        return;
-    }
-
-    gis('sexy breasts', (error, results) => {
-            if (error) {
-                console.log(error);
-            }
-            else {
+    if(['!bewwwwbs', '!bewwwbs', 'bewwbs!', 'bewwwbs'].indexOf(receivedMessage.content) > -1)  {
+        gis('sexy breasts', (error, results) => {
+            const randResult = results[Math.floor(Math.random() * results.length)];
+            receivedMessage.channel.send('BEWWWWWWWWBS!', { files: [randResult.url] });
+        });
+    } else if(receivedMessage.content === '!flip') {
+        if(Math.random() < 0.5) {
+            gis('heads', (error, results) => {
                 const randResult = results[Math.floor(Math.random() * results.length)];
-                receivedMessage.channel.send('BEWWWWWWWWBS!', { files: [randResult.url] });
-            }
+                receivedMessage.channel.send('HEADS!!', { files: [randResult.url] });
+            });
+        } else {
+            gis('tails', (error, results) => {
+                const randResult = results[Math.floor(Math.random() * results.length)];
+                receivedMessage.channel.send('TAILS!!', { files: [randResult.url] });
+            });
         }
-    );
+    } else if(receivedMessage.content === '!roll') {
+        const num = 1 + Math.floor(Math.random()*6);
+        receivedMessage.channel.send(num);
+    }  else if(receivedMessage.content.startsWith('!insult ')) {
+        const insult = https.get('https://evilinsult.com/generate_insult.php?lang=en&type=text', (resp) => {
+            let data = '';
+          
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+              data += chunk;
+            });
+          
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+              receivedMessage.channel.send(receivedMessage.content.substr(8) + ': ' + data);
+            });
+          
+          });
+        
+    }/*else if(receivedMessage.content === '!instul') {
+        receivedMessage.channel.send(1 + Math.floor(Math.random()*6));
+        gis('heads', (error, results) => {
+            const randResult = results[Math.floor(Math.random() * results.length)];
+            receivedMessage.channel.send('BEWWWWWWWWBS!', { files: [randResult.url] });
+        });
+    }*/
 })
 
 
-const bot_secret_token = "YOUR TOKEN";
+const bot_secret_token = "NzI0MjUzNjI2NzU5MTE4ODg5.Xu9ujQ.AjT9-e7twBEJ8s8EpxhgpISIbRc";
 client.login(bot_secret_token);
